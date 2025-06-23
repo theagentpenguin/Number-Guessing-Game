fb8f179fc2cfdcfca0c531ddb633c6f682c441ad
theagentpenguin <gunaseelanabcd@gmail.com>
2025-06-23 23:26:38 +0530

    trying with commander

--- numGuessGame.js
+++ numGuessGame.js
@@ -1,1 +1,21 @@
-console.log("Let's Goooo!!!!!");
+#!/usr/bin/env node
+import {Command} from 'commander';
+
+const game = new Command();
+game
+    .name('Number Guess Game')
+    .description('A CLI based game with commander.js')
+    .option('-d, --debug','to debug')
+    .option('-p, --play','Starts the game');
+game.parse();
+
+game
+    .command('play')
+    .action(()=>{
+        console.log("Inside the play area");
+    });
+game.parse();
+const options = game.opts();
+if(options.debug){
+    console.log("The game shall start!");
+}
