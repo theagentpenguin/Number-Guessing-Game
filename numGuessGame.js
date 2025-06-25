2e213ea959774019957eb72823018cf57eb3c024
theagentpenguin <gunaseelanabcd@gmail.com>
2025-06-25 21:24:17 +0530

    changing approach

--- numGuessGame.js
+++ numGuessGame.js
@@ -1,25 +1,45 @@
 #!/usr/bin/env node
-import {Command} from 'commander';
+
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+import gradient from 'gradient-string';
 import readline from 'readline';
+import chalkAnimation from 'chalk-animation';
+import figlet from 'figlet';
 
-const game = new Command();
-game
-    .name('Number Guess Game')
-    .description('A CLI based game with commander.js')
-    .option('-d, --debug','to debug')
-    .option('-p, --play','Starts the game');
-game.parse();
+const sleep = (ms = 3000) => new Promise((r)=> setTimeout(r,ms));
 
-const randomNumber = Math.floor(Math.random());
+let userGuess;
+async function game(){
+    const welcomeMessage = chalkAnimation.karaoke(
+        'Welcome to NumGuess, a number guessing game! \n'
+    );
+    await sleep();
+    console.log(`
+        ${chalk.blue('How to Play???')}
+        1] Select the difficulty that you want to play with
+        2] Guess the random number
+        3] You'll win if you guess the correct number before your chances run out!!
 
-
-game
-    .command('play')
-    .action(()=>{
-        console.log("Select the mode you want to play!");
-    });
-game.parse();
-const options = game.opts();
-if(options.debug){
-    console.log("The game shall start!");
+        `);
 }
+
+async function numGuess(){
+    const guesses = await inquirer.prompt({
+        num : 'guessed_number',
+        type: 'input',
+        message: 'Enter your guess',
+        default(){
+            return 'any number between 0-100';
+        },
+
+    });
+    userGuess = guesses.guessed_number;
+    console.log(userGuess);
+}
+
+async function solutionCheck(){
+
+}
+//await game();
+await numGuess();
